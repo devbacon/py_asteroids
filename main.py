7112bc7c8f522571d3eab4e35706f11068803e99
devbacon <devon.bacon@gmail.com>
2025-12-21 17:51:06 -0500

    pace screen generation to 60 fps

--- main.py
+++ main.py
@@ -9,13 +9,17 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    while True:
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True: # Main game loop
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return  # Exit the game loop when the window is closed
         screen.fill((0, 0, 0))  # Clear the screen with black color
         pygame.display.flip()  # Update the display
+        dt = clock.tick(60) / 1000  # Limit the frame rate to 60 FPS. Store delta as milliseconds
 
 if __name__ == "__main__":
     main()
