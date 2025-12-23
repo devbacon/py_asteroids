0811ce4a153c1aab18abdf32913e04817daf50bd
devbacon <devon.bacon@gmail.com>
2025-12-22 20:27:48 -0500

    add turn controls

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -8,6 +9,7 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     clock = pygame.time.Clock()
     dt = 0
@@ -18,8 +20,11 @@ def main():
             if event.type == pygame.QUIT:
                 return  # Exit the game loop when the window is closed
         screen.fill((0, 0, 0))  # Clear the screen with black color
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()  # Update the display
         dt = clock.tick(60) / 1000  # Limit the frame rate to 60 FPS. Store delta as milliseconds
+
 
 if __name__ == "__main__":
     main()
