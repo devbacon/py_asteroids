21115fbc50b514de592da6b287f8fc9f9753f1a5
devbacon <devon.bacon@gmail.com>
2025-12-23 11:07:50 -0500

    group game objects for more clean main loop

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     clock = pygame.time.Clock()
@@ -20,8 +23,11 @@ def main():
             if event.type == pygame.QUIT:
                 return  # Exit the game loop when the window is closed
         screen.fill((0, 0, 0))  # Clear the screen with black color
-        player.update(dt)
-        player.draw(screen)
+
+        updatable.update(dt)
+        for draw_item in drawable:
+            draw_item.draw(screen)
+            
         pygame.display.flip()  # Update the display
         dt = clock.tick(60) / 1000  # Limit the frame rate to 60 FPS. Store delta as milliseconds
 
